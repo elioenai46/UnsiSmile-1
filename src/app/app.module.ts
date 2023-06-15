a3052fdabd21c87cefbda5dc2475ab664369f4db
carlosaP131 <carlos.cantrax303@gmail.com>
2023-06-15 16:35:53 -0600

    Creado componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatedraticosComponent } from './componente/catedraticos/catedraticos.component';
+import { AlumnosComponent } from './componente/alumnos/alumnos.component';
+import { AdministradoresComponent } from './componente/administradores/administradores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatedraticosComponent,
+    AlumnosComponent,
+    AdministradoresComponent
   ],
   imports: [
     BrowserModule,
--- src/app/fservice/catedraticos.service.ts
+++ src/app/fservice/catedraticos.service.ts
@@ -1,9 +1,29 @@
+import{ HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CatedraticoService {
-
-  constructor() { }
+    //Variable privada para enlazar el back con el from
+    private baseUrl='http://localhost:8080/unsis/';
+    //pasar el httpclient como parametro en el constructor
+    constructor(private http:HttpClient) { }
+    //metodos que se enlazan con el back 
+    getCatedraticos():Observable<any>{
+      return this.http.get(`${this.baseUrl}`+'listarTodosCatedraticos');
+    }
+    createCatedraticos(catedratico:object):Observable<object>{
+      return this.http.post(`${this.baseUrl}'+'crearCatedraticos`,catedratico);
+    }
+    deleteCatedraticos(id:number):Observable<any>{
+      return this.http.delete(`${this.baseUrl}eliminarCatedraticos/${id}`,{responseType:'text'})
+    }
+    getCatedraticosId(id:number):Observable<object>{
+      return this.http.get(`${this.baseUrl}/obtenerCatedratico/${id}`)
+    }
+    upateCatedraticosId(id:number, value: any):Observable<object>{
+      return this.http.get(`${this.baseUrl}/actualizarCatedraticos/${id}`, value);
+    }
 }
