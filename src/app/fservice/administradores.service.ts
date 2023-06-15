d987c5f7992718b0be3d0d99c13e00779643123c
Tu Nombre <you@example.com>
2023-06-15 16:43:54 -0600

    Service Administradores

--- src/app/fservice/administradores.service.ts
+++ src/app/fservice/administradores.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdministradoresService {
+  private baseUrl = 'http://localhost:8080/unsis/';
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getAdministradores(): Observable<any> {
+    return this.http.get('${this.baseUrl}'+'listarTodosAdministradores');
+  }
+  createAdministrador(administradores: object): Observable<object> {
+    return this.http.get('${this.baseUrl}'+'crearAdministrador');
+  }
+
+  deleteAdministrador(id_admin: Number): Observable<any> {  
+    return this.http.delete(`${this.baseUrl}eliminarAdministrador/${id_admin}`, { responseType: 'text' });  
+  }  
+  
+  getAdministradorId(idAdministrador: Number): Observable<Object> {  
+    return this.http.get(`${this.baseUrl}/obtenerAdministradorPorId/${idAdministrador}`);  
+  }  
+  
+  updateAdministrador(id: number, value: any): Observable<Object> {  
+    return this.http.post(`${this.baseUrl}/actualizarRegistro/${id}`, value);  
+  }  
 }
+
